87647e0ff2263a7e101c266a92e095b282baeef2
Jonny Gamba <gambonny@gmail.com>
2025-01-28 18:01:52 -0500

    add github-actions reporter when running on CI

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,6 @@ export default defineConfig({
     coverage: {
       exclude: ["**/coverage/**", ...coverageConfigDefaults.exclude],
     },
+    reporters: process.env.GITHUB_ACTIONS ? ["dot", "github-actions"] : ["dot"],
   },
 });
